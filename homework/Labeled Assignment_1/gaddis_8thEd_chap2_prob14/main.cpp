b0579942f7bbdca652940dc8c81f42d41b4ce6bb
nick625 <mr.mr562@yahoo.com>
2016-03-04 11:46:20 -0800

    Signed-off-by: nick625 <mr.mr562@yahoo.com>

---
+++ homework/Labeled Assignment_1/gaddis_8thEd_chap2_prob14/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File: display the pieces of information
+ * Author: nicolas Gamez
+ * Created on March 3, 2016 
+ */
+
+//system Libraries
+#include <iostream>
+using namespace std;
+
+//user Libraries
+int main(int argc, char** argv) {
+    cout<<"your name \n"
+    <<"your address,with city,state,and zip code \n"
+    <<"your telephone number \n"
+    <<"'your college major \n";
+    return 0;
+}
+
