ee14f123e56b8eb310b7c7c6773f32f166215ed9
nick625 <mr.mr562@yahoo.com>
2016-03-03 16:52:45 -0800

    Signed-off-by: nick625 <mr.mr562@yahoo.com>

---
+++ homework/savitch_9thed_chap1_prob2/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File: prints outs "CS!"
+ * Author: Nicolas Gamez
+ * Created on March 3, 2016, 3:19 PM
+ */
+
+//systeam Libraries
+#include <iostream>
+using namespace std;
+
+//user Libraries
+int main(int argc, char** argv) {
+    for (int i = 0; i<20; i++)
+    cout<<"*";
+cout<<"\n";
+cout<<"\n";
+
+cout<<"  ccc  "<<"   ssss   !!\n";
+cout<<" c    c"<<" s    s   !!\n";
+cout<<"c      "<<"s         !!\n";
+cout<<"c      "<<" s        !!\n";
+cout<<"c      "<<"  ssss    !!\n";
+cout<<" c     "<<"      s   !!\n";
+cout<<"  c    "<<"       s  !!\n";
+cout<<"   c  c"<<" s    s   !!\n";
+cout<<"    cc "<<"  sss     !!\n";
+
+cout<<"\n";
+for (int i=0; i< 20; i++)
+    cout<<"*";
+
+cout<<"\n";
+cout<<"Computer Science is Cool Stuff!!!";
+    return 0;
+}
+
---
+++ homework/savitch_9thed_chap1_prob3/main (2).cpp
@@ -0,0 +1,24 @@
+/* 
+ * File: enter numbe of quarters,dimes,and nickels 
+ * Author: Nicolas Gamez
+ * Created on March 3, 2016, 3:55 PM
+ */
+//systeam Libraries
+#include <iostream>
+using namespace std;
+
+//user Libraries
+int main ( )
+{ 
+int quart, dim, nick; 
+double pennies; 
+cout<<"Enter quarters:"; 
+cin>>quart; 
+cout<<"Enter dimes:"; 
+cin>>dim; 
+cout<<"Enter nickels"; 
+cin>>nick; 
+pennies = 5*nick + 10*dim + 25*quart; 
+cout<<"\nThe number of pennis are :"<<pennies; 
+return 0;
+}
