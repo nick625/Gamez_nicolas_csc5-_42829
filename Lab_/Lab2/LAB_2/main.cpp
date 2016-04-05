06c5e1038f46577569831c0a933656ad110ab9a3
nick625 <mr.mr562@yahoo.com>
2016-04-05 13:28:13 -0700

    Signed-off-by: nick625 <mr.mr562@yahoo.com>

---
+++ Lab_/Lab2/LAB_2/main.cpp
@@ -0,0 +1,43 @@
+/* 
+ * File:  grade if 
+ * Author: Nicolas gamez
+ * Created on March 23, 2016, 9:30 AM
+ */
+
+//system libraries
+#include <iostream>
+using namespace std;
+
+//user libraries
+
+//Global constants
+
+//funcation prototypes 
+
+//Execution being here!
+int main(int argc, char** argv) {
+    float A_score(90);
+    float B_score(80);
+    float C_score(70);
+    float D_score(60);
+    
+    int test; //test score
+    
+    //input test score
+    cout<<"Enter test score from 0-100 for letter grade"<<endl;
+    cin>>test;
+    
+    //determine the letter grade
+    if (test >=A_score)
+        cout<<"your grade is A.";
+    else if(test>=B_score)
+        cout<<"your grade is B";
+    else if(test>=C_score)
+        cout<<"your grade is C";
+    else if(test>=D_score)
+        cout<<"your grade is D";
+    else
+        cout<<"your grade is F";
+    return 0;
+}
+
---
+++ Lab_/hollo world/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: nic
+ * Created on February 23, 2016, 4:59 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
