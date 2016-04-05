6bc94cfe71dbd0aaa60339dc29f825bbafb3df70
nick625 <mr.mr562@yahoo.com>
2016-04-05 13:23:15 -0700

    Signed-off-by: nick625 <mr.mr562@yahoo.com>

---
+++ New folder/Holla_world_v2/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Nicolas Gamez
+ *Purpose: Our first program
+ * Created on February 17, 2016, 9:45 AM
+ */
+
+//system Libraries 
+#include <iostream>
+using namespace std;
+
+//user Libraries
+
+//Global constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+   //Declare variables 
+
+   //Initialize variables
+
+   //Input values 
+   
+   //Maps the inputs to the outputs
+    
+    //output the results
+    cout<<"Hello World"<<endl;
+    
+    //Exit Stage Right! 
+    return 0;
+}
+
